518a7875bdea48eecb980a2a9342e20b1b61f4da
pedram azimzadeh <azimzadeh.pedram@gmail.com>
2022-01-22 01:12:07 -0500

    R code _ Viz done

--- Time_Series_R (Energy Consumption data).R
+++ Time_Series_R (Energy Consumption data).R
@@ -1,7 +1,7 @@
 
 # Import the dataset 
 # Make "Date" variable row names 
-ts <- read.csv("C:/Users/azimz/Desktop/Time-Series_R/opsd_germany_daily.csv", header = TRUE, row.names = "Date")
+ts <- read.csv("C:/Users/azimz/Desktop/Time-Series_R/Time-Series_R/opsd_germany_daily.csv", header = TRUE, row.names = "Date")
 # Explore and summarize the data
 str(ts)
 dim(ts)
@@ -16,7 +16,7 @@ tail(ts)
 ts[c("2006-01-01", "2006-01-04"), ]
 
 # For visualization purposes we keep "Date" as a column 
-tsd <- read.csv("C:/Users/azimz/Desktop/Time-Series_R/opsd_germany_daily.csv", header = TRUE)
+tsd <- read.csv("C:/Users/azimz/Desktop/Time-Series_R/Time-Series_R/opsd_germany_daily.csv", header = TRUE)
 summary(tsd)
 # Correct the data type for Date 
 tsd$Date <- as.Date(tsd$Date)
@@ -32,7 +32,16 @@ tsd$year <- as.numeric(format(tsd$Date, "%Y"))
 plot(tsd$year, tsd$Consumption, type = "l", xlab = "Year", ylab = "Energy Consumption",
      lty = 1, xlim = c(2006, 2017), ylim = c(800, 1700))
 
+plot(tsd$Consumption, 
+     xlim = c(2006, 2017), ylim = c(800, 1700), type = "l",
+     xlab = "Year", ylab = "Consumption",
+     main = "Energy consumption in Germany")
 
+# use log values of consumption and take differences of log 
+plot(10*diff(log(tsd$Consumption)), type = "l", col = "orange", 
+     main = "Energy consumption in Germany",
+     ylab = "Consumption",
+     ylim = c(-5, 5))
 
 
 
